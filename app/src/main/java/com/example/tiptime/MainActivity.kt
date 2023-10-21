4d8da8e2c7bf4f8fc077dcc6c921da769ea0cbb0
andihasan97 <andihasanelfalakiy@gmail.com>
2023-10-21 12:55:11 +0700

    Menggunakan fungsi remember untuk menyimpan status

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -28,6 +28,9 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -84,10 +87,10 @@ fun TipTimeLayout() {
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    var amountInput: MutableState<String> = mutableStateOf("0")
+    var amountInput by remember { mutableStateOf(" ") }
     TextField(
-        value = amountInput.value,
-        onValueChange = { amountInput.value = it },
+        value = amountInput,
+        onValueChange = { amountInput = it },
         modifier = modifier
     )
 }
